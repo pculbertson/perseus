916351bb55760cc7ac4b6ee4ff7faf41424a7bc8
Albert Li <alberthli@caltech.edu>
2024-08-24 11:25:27 -0700

    updated data visualization script

--- data/visualize_data.py
+++ data/visualize_data.py
@@ -1,48 +1,43 @@
 from pathlib import Path
 from typing import Union
 
-import h5py
 import matplotlib.pyplot as plt
 
 from perseus import ROOT
+from perseus.detector.data import KeypointDataset, KeypointDatasetConfig
 
 
-def main(hdf5_path: Union[str, Path], mode: str = "images") -> None:
+def main(hdf5_path: Union[str, Path], mode: str = "train", img_type: str = "image") -> None:
     """Main function."""
-    assert mode in ["images", "depth_images", "segmentation_images"], f"Invalid mode: {mode}"
+    assert img_type in ["image", "depth_image", "segmentation_image"], f"Invalid img_type: {img_type}"
     num_keypoints = 8
     keypoint_colormap = plt.cm.get_cmap("tab10", num_keypoints)
+    cfg = KeypointDatasetConfig(dataset_path=str(hdf5_path))
+    dataset = KeypointDataset(cfg, train=True if mode == "train" else False)
 
-    with h5py.File(hdf5_path, "r") as f:
-        # max/min pixel coordinates over whole dataset
-        max_pixel_coordinates = f["train"]["pixel_coordinates"][:].max()
-        min_pixel_coordinates = f["train"]["pixel_coordinates"][:].min()
-        print(f"Max pixel coordinates: {max_pixel_coordinates}")
-        print(f"Min pixel coordinates: {min_pixel_coordinates}")
+    for i, example in enumerate(dataset):
+        if img_type == "image":
+            image = example["image"].permute(1, 2, 0)
+        elif img_type == "depth_image":
+            image = example["depth_image"]
+        elif img_type == "segmentation_image":
+            image = example["segmentation_image"]
+        keypoints = example["pixel_coordinates"]
+        # print(f"Max pixel coordinates: {keypoints.max()}")
+        # print(f"Min pixel coordinates: {keypoints.min()}")
 
-        # visualizing the training data
-        for i in range(len(f["train"][f"{mode}"])):
-            print(f"Image {i}")
-            images = f["train"][f"{mode}"][i]
-            keypoints_all = f["train"]["pixel_coordinates"][i]
-
-            for j in range(len(images)):
-                print(f"    Frame {j}")
-                image = images[j]  # (256, 256, 3)
-                keypoints = keypoints_all[j]  # (8, 2)
-
-                # Plot the image and keypoints
-                plt.imshow(image)
-                for k in range(len(keypoints)):
-                    plt.scatter(keypoints[k, 0], keypoints[k, 1], color=keypoint_colormap(k), marker="o")
-                plt.title(f"Image {i}/{len(f['train'][f'{mode}'])} | Frame {j}/{len(images)}")
-                plt.show()
+        plt.imshow(image)
+        for k in range(len(keypoints)):
+            plt.scatter(keypoints[k, 0], keypoints[k, 1], color=keypoint_colormap(k), marker="o")
+        plt.title(f"Image {i}/{len(dataset)}")
+        breakpoint()
+        plt.show()
 
 
 if __name__ == "__main__":
-    hdf5_path = Path(f"{ROOT}/data/merged/merged.hdf5")
+    hdf5_path = Path(f"{ROOT}/data/merged_lazy/merged.hdf5")
 
     # uncomment the one you want to see
-    main(hdf5_path, mode="images")
-    # main(hdf5_path, mode="depth_images")
-    # main(hdf5_path, mode="segmentation_images")
+    main(hdf5_path, mode="test", img_type="image")
+    # main(hdf5_path, img_type="depth_image")
+    # main(hdf5_path, img_type="segmentation_image")
